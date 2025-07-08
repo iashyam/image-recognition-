953bcd5d02ee3723fe45aaec2e5eb3a76a18ce25
iashyam <shyam10kwd@gmail.com>
2025-07-08 20:32:36 +0530

    updates gitignore

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ network.eval()
 # eff_net_LABELS = {int(k):v for k, v in eff_net_LABELS.items()}
 
 eff_net_LABELS = labels
-def recoganize_image(image):
+def recoganize_image(image: Image.Image):
 	image_transfomed = eff_net_transforms(image).unsqueeze(0)
 	with torch.no_grad():
 		pred = network(image_transfomed)
@@ -33,16 +33,16 @@ def image_from_a_link(link: str):
     image = Image.open(BytesIO(response.content))
     return image 
 	
+if __name__ == "__main__":
+	link = "https://imgs.search.brave.com/uiwpUrnRL20-jsinT_ii5Ri0U_AHw8R8KINqJVEtNL0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjQv/NTcwLzczOS9zbWFs/bC9wb3J0cmFpdC1v/Zi1hLWhhcHB5LXdl/bHNoLWNvcmdpLWRv/Zy1pbi1hdXR1bW4t/Zm9yZXN0LWFpLWdl/bmVyYXRlZC1mcmVl/LXBob3RvLmpwZw"
 
-link = "https://imgs.search.brave.com/uiwpUrnRL20-jsinT_ii5Ri0U_AHw8R8KINqJVEtNL0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjQv/NTcwLzczOS9zbWFs/bC9wb3J0cmFpdC1v/Zi1hLWhhcHB5LXdl/bHNoLWNvcmdpLWRv/Zy1pbi1hdXR1bW4t/Zm9yZXN0LWFpLWdl/bmVyYXRlZC1mcmVl/LXBob3RvLmpwZw"
+	link = input("Enter link: ")
+	def recoganize_image_from_link(link: str):
+		image = image_from_a_link(link)
+		predicted = recoganize_image(image)
+		plt.title(f"Predicted: {predicted} ")
+		plt.imshow(image)
+		plt.axis('off')
+		plt.show()
 
-link = input("Enter link: ")
-def recoganize_image_from_link(link: str):
-	image = image_from_a_link(link)
-	predicted = recoganize_image(image)
-	plt.title(f"Predicted: {predicted} ")
-	plt.imshow(image)
-	plt.axis('off')
-	plt.show()
-
-recoganize_image_from_link(link)
+	recoganize_image_from_link(link)
